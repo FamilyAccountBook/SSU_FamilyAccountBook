ad6d911c8318500f0152aaf96261f6ff109d8017
rk213 <kyoung20218@soongsil.ac.kr>
2025-11-12 21:00:25 +0900

    main,AndroidManifest(#6)

--- app/src/main/java/com/example/ui_familybook/MainActivity.java
+++ app/src/main/java/com/example/ui_familybook/MainActivity.java
@@ -1,24 +1,22 @@
 package com.example.ui_familybook;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        setContentView(R.layout.activity_main); // 기본 레이아웃
+
+        // 앱 켜지자마자 로그인 화면으로 이동
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+
+        // MainActivity는 필요 없으니까 종료 (뒤로가기 눌러도 안 보이게)
+        finish();
     }
 }
