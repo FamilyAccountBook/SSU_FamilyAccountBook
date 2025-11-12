11b76c3b99c7d9f7cf80eb634ccf23968767233e
arin <leesch587@naver.com>
2025-11-13 02:39:12 +0900

    feat. 바텀내비게이션(#7)

--- app/src/main/java/com/example/ui_familybook/HomeFragment.java
+++ app/src/main/java/com/example/ui_familybook/HomeFragment.java
@@ -1,7 +1,30 @@
 package com.example.ui_familybook;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import com.example.ui_familybook.databinding.FragmentHomeBinding;
 
 class HomeFragment extends Fragment {
 
+    private FragmentHomeBinding binding;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    // binding.을 통해 fragment_home.xml 안의 뷰들에 접근 가능
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
 }
--- app/src/main/java/com/example/ui_familybook/MainActivity.java
+++ app/src/main/java/com/example/ui_familybook/MainActivity.java
@@ -7,6 +7,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import com.example.ui_familybook.databinding.ActivityMainBinding;
 import com.google.android.material.bottomnavigation.BottomNavigationItemView;
@@ -24,6 +27,18 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
 
         setContentView(binding.getRoot());
+
+        BottomNavigationView bottomNav = binding.bottomNav;
+
+        NavHostFragment navHostFragment =(NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.main_frm);
+
+        // 실제 화면 이동(navigation)을 제어하는 'NavController'
+        NavController navController= navHostFragment.getNavController();
+
+        // 'NavigationUI' 사용해 'bottomNav'와 'navController'를 연결
+        NavigationUI.setupWithNavController(bottomNav, navController);
+
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
