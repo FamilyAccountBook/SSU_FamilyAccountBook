e8765a461155dfefb46e126c075d6812ec30a3b7
Jun Moon <munjun7312@gmail.com>
2025-11-12 19:56:24 +0900

    moon

---
+++ AndroidStudioProjects/famplus1/app/src/main/java/com/example/famplus1/CategoryItem.java
@@ -0,0 +1,11 @@
+package com.example.famplus1;
+
+public class CategoryItem {
+    public String emoji;
+    public String name;
+
+    public CategoryItem(String emoji, String name) {
+        this.emoji = emoji;
+        this.name = name;
+    }
+}
